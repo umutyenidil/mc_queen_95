26262b0dd22bf6a7edd5e8559bb815d4f9970795
Umut Yenidil <umut.yenidil9@gmail.com>
2024-05-10 15:09:15 +0300

    init()

---
+++ android/app/src/main/kotlin/com/example/mc_queen_95/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mc_queen_95
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:mc_queen_95/views/pages/home/home.dart';
+
+void main() {
+  runApp(
+    const GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    ),
+  );
+}
